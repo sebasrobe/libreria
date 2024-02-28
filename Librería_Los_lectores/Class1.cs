93e37406f9be6212b6dc601fe0debf94f81c1190
unknown <sabnait@hotmail.com>
2024-02-28 10:47:01 -0500

    se agrega el proyecto

---
+++ Librería_Los_lectores/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Librería_Los_lectores
+{
+    class Class1
+    {
+    }
+}
